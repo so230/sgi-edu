552af26b2cbeb93076cacc1ee5cec0c1667ca363
Hojin Lee <hjlee@172.20.nate.com>
2023-05-18 18:04:11 +0900

    add webhook for github

---
+++ cd/cd.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#자동 배포 및 실행 프로세스 
+
+#git push
+cd ~/apps/sgi-edu
+git push
+rm sgi-edu.tar
+tar -cf ~/apps/sgi-edu.tar ~/apps/sgi-edu
+ssh -i /home/ec2-user/keys/private_was.pem ubuntu@ip rm /home/ubuntu/cd/sgi-edu.tar
+scp -i /home/ec2-user/keys/private_was.pem ubuntu@ip sgi-edu.tar ubuntu@10.0.3.225:/home/ubuntu/cd
+ssh -i /home/ec2-user/keys/private_was.pem ubuntu@ip tar -xf 
+
+# scp -i ./sgi-edu-3.pem ./private_was.pem ec2-user@ec2-15-165-75-155.ap-northeast-2.compute.amazonaws.com:/home/ec2-user/keys
--- monolithic/server.js
+++ monolithic/server.js
@@ -1,11 +1,33 @@
 var express = require('express');
 const fs = require('fs');
+const { exec} = require('child_process');
+
+
 var app = express();
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
 // use res.render to load up an ejs view file
+
+//git webhoook api
+app.get('/webhook/master/push', function(req,res){
+  exec("ls -al", (error, stdout, stderr) => {
+
+    if(error){
+      console.log(`error: ${error.message}`);
+      return;
+    }
+
+    if(stderr){
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+
+    console.log(`stdout: ${stdout}`);
+  })
+  res.send("ci/cd를 실행합니다.");
+});
 
 // index page
 app.get('/', function(req, res) {
