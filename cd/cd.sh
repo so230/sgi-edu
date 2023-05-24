b935ab9e7a0754703cf59d34275e8bd48bcb3aa0
sso230 <sso230@hotmail.com>
2023-05-25 08:55:26 +0900

    change cd.sh

--- cd/cd.sh
+++ cd/cd.sh
@@ -7,8 +7,8 @@ cd ~/apps/sgi-edu
 git pull
 echo '소스가 정상 다운로드 되었습니다'
 echo '빌드를  시작 합니다.'
-#npm build 
-ehco '빌드를 완료 하였습니다.'
+#npm build
+echo '빌드를 완료 하였습니다.'
 echo 'test 시작'
 #npm run test
 echo 'test 완료'
@@ -16,10 +16,11 @@ echo '배포 시작'
 
 cd ~/apps
 tar -cf sgi-edu.tar ~/apps/sgi-edu
-#ssh -i /home/ec2-user/keys/private_was.pem ubuntu@ip rm /home/ubuntu/cd/sgi-edu.tar
-#scp -i /home/ec2-user/keys/private_was.pem ubuntu@ip sgi-edu.tar ubuntu@10.0.3.225:/home/ubuntu/cd
-#ssh -i /home/ec2-user/keys/private_was.pem ubuntu@ip tar -xf sgi-edu.tar
-#ssh -i /home/ec2-user/keys/private_was.pem pm2 restart server
+ssh -i /home/ec2-user/keys/private_was.pem ubuntu@10.0.3.225 rm /home/ubuntu/cd/sgi-edu.tar
+scp -i /home/ec2-user/keys/private_was.pem sgi-edu.tar ubuntu@10.0.3.225:/home/ubuntu/cd
+ssh -i /home/ec2-user/keys/private_was.pem ubuntu@10.0.3.225 tar -xf sgi-edu.tar
+ssh -i /home/ec2-user/keys/private_was.pem ubuntu@10.0.3.225 cp -rf /home/ubuntu/cd/sgi-edu/monolithic/* /home/ubuntu/apps/sgi-edu/monolithic/
+ssh -i /home/ec2-user/keys/private_was.pem ubuntu@10.0.3.225 pm2 restart server
 echo '배포 완료'
 
 # scp -i ./sgi-edu-3.pem ./private_was.pem ec2-user@ec2-15-165-75-155.ap-northeast-2.compute.amazonaws.com:/home/ec2-user/keys
