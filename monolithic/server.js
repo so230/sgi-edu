0619bf4fd5f19e97865ef95f674ad9844f4e8978
sso230 <sso230@hotmail.com>
2023-05-25 10:16:41 +0900

    add urlencode

--- monolithic/server.js
+++ monolithic/server.js
@@ -6,6 +6,8 @@ const { exec} = require('child_process');
 var app = express();
 app.use(express.json());
 
+app.use(express.urlencoded({ extended: true })); // support encoded bodies
+
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
