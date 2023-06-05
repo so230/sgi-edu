eb51b20e1ee3da8845d2d448b645ec2314dac1e6
sso230 <sso230@hotmail.com>
2023-06-05 15:39:28 +0900

    spring boot init

--- msa/member-service/src/main/java/sgi/edu/deom/memberService/MemberServiceApplication.java
+++ msa/member-service/src/main/java/sgi/edu/deom/memberService/MemberServiceApplication.java
@@ -1,13 +1,26 @@
 package sgi.edu.deom.memberService;
 
+import org.springframework.boot.*;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.*;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
 
-@SpringBootApplication
+@RestController
+@EnableAutoConfiguration
 public class MemberServiceApplication {
+
+	// 이호진, 기본 Root API, health Check 용 입니다.
+	@RequestMapping("/member")
+    String home() {
+        return "Hello World!";
+    }
+
+
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(MemberServiceApplication.class, args);
 	}
-
 }
