7981b24308f8abd5d76c22f3c69458e20803a436
sso230 <sso230@hotmail.com>
2023-07-06 16:04:18 +0900

    d

--- msa/company-service/server.js
+++ msa/company-service/server.js
@@ -9,16 +9,20 @@ app.use(express.json());
 app.use(cors());
 
 app.use(express.urlencoded({ extended: true })); // support encoded bodies
+const {
+  createMechanism,
+} = require("@jm18457/kafkajs-msk-iam-authentication-mechanism");
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
-
+const brokersEndpoint = 'b-3.sgidemomsk.o9fr30.c4.kafka.ap-northeast-2.amazonaws.com:9098,b-1.sgidemomsk.o9fr30.c4.kafka.ap-northeast-2.amazonaws.com:9098,b-2.sgidemomsk.o9fr30.c4.kafka.ap-northeast-2.amazonaws.com:9098';
 // use res.render to load up an ejs view file
 const kafka = new Kafka({
   clientId: 'company',
-  brokers: ['b-3.sgidemomsk.o9fr30.c4.kafka.ap-northeast-2.amazonaws.com:9092'
-  ,'b-1.sgidemomsk.o9fr30.c4.kafka.ap-northeast-2.amazonaws.com:9092'
-  ,'b-2.sgidemomsk.o9fr30.c4.kafka.ap-northeast-2.amazonaws.com:9092'],
+  brokers: brokersEndpoint.split(','),
+  ssl: true,
+  sasl: createMechanism({ region: 'ap-northeast-2' }),
+
 })
 
 const producer = kafka.producer()
