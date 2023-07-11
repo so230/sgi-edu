0d9b34de214c722fcad7f4d9d566513786435046
sso230 <sso230@hotmail.com>
2023-07-11 15:36:20 +0900

    a

--- msa/company-service/server.js
+++ msa/company-service/server.js
@@ -84,8 +84,8 @@ app.post('/company/:id/:name', async function(req, res){
         value: message.value.toString(),
       })
 
-      const command = value.toString().split(':')[0];
-      const id = value.toString().split(':')[1];
+      const command = message.value.toString().split(':')[0];
+      const id = message.value.toString().split(':')[1];
 
       if(command == "commit"){
         commitTxCompany(id)
